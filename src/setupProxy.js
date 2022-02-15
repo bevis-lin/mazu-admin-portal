892c501063ae9a8c2630bca4cccb335b69352007
Bevis Lin <bevis.lin@icloud.com>
2022-02-15 14:22:25 +0800

    use proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/',
+    '/login',
     createProxyMiddleware({ target: 'https://api.testnet.sentimen.art/' })
   );
 };
