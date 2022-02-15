64ca973efdc24f8b1007910e816e63680f3d7fa7
Bevis Lin <bevis.lin@icloud.com>
2022-02-15 14:49:45 +0800

    add api prefix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
-    createProxyMiddleware({ target: 'https://api.testnet.sentimen.art/' })
+    '/api/*',
+    createProxyMiddleware({
+      target: 'https://api.testnet.sentimen.art/',
+      changeOrigin: true,
+    })
   );
 };
